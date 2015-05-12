de297370e56f97bb15bf0ea12598f0fc4fcc269f
Marcin Ossowski <marcin.ossowski92@gmail.com>
2015-05-12 14:58:09 +0200

    Add project model

---
+++ async-rest/src/main/java/com/misys/async_rest/model/Project.java
@@ -0,0 +1,24 @@
+package com.misys.async_rest.model;
+
+import javax.xml.bind.annotation.XmlRootElement;
+
+@XmlRootElement
+public class Project {
+
+	private String id;
+	private String name;
+	
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+}
